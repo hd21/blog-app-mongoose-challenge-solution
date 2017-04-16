125f498f2f67245d4ce5e33b270385633ba6d646
hd21 <hazelmarie.doctor@gmail.com>
2017-04-16 12:46:11 -0400

    Added updated dependencies

--- models.js
+++ models.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
 
 const blogPostSchema = mongoose.Schema({
-  author: {
-    firstName: String,
-    lastName: String
-  },
-  title: {type: String, required: true},
-  content: {type: String},
-  created: {type: Date, default: Date.now}
+    author: {
+        firstName: String,
+        lastName: String
+    },
+    title: { type: String, required: true },
+    content: { type: String },
+    created: { type: Date, default: Date.now }
 });
 
 
 blogPostSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();
+    return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
 blogPostSchema.methods.apiRepr = function() {
-  return {
-    id: this._id,
-    author: this.authorName,
-    content: this.content,
-    title: this.title,
-    created: this.created
-  };
+    return {
+        id: this._id,
+        author: this.authorName,
+        title: this.title,
+        content: this.content,
+        created: this.created
+    };
 }
 
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
-module.exports = {BlogPost};
+module.exports = { BlogPost };
